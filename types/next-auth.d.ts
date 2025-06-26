55f1e397a302ef859835701dabe46dbcc6dcd30f
vic27l <vicleandro27@gmail.com>
2025-06-26 12:27:03 -0300

    Update next-auth.d.ts

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,11 +1,10 @@
 // types/next-auth.d.ts
+import 'next-auth';
 
-import NextAuth from "next-auth";
-
-declare module "next-auth" {
+declare module 'next-auth' {
   interface Session {
     user: {
-      id: string;
+      id: string; // Adicionamos o ID do usuário aqui
       name?: string | null;
       email?: string | null;
       image?: string | null;
@@ -13,15 +12,6 @@ declare module "next-auth" {
   }
 
   interface User {
-    id: string;
-    name?: string | null;
-    email?: string | null;
-    image?: string | null;
-  }
-}
-
-declare module "next-auth/jwt" {
-  interface JWT {
     id: string;
   }
 }
